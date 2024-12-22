3efeee56a1696ca1162d2448d77103682b875f05
Joshua Noyes <Koholos@yahoo.com>
2024-12-22 16:08:02 -0500

    working test for valid winning move

--- TTT.js
+++ TTT.js
@@ -5,23 +5,27 @@ function db(input) {
 }
 
 
+
 //Player logic
 function createPlayer(name, screenOrder){
     const symbol = screenOrder === 1?"X":"O";
+
     return{name,symbol,screenOrder}
 }
 
 //Board logic
 function createBoard(){
-    const board =  {b1:"1",b2:"2",b3:"3",
-                    b4:"4",b5:"5",b6:"6",
-                    b7:"7",b8:"8",b9:"9"};
+    const board =  ["1","2","3",
+                    "4","5","6",
+                    "7","8","9"];
     
-    return board
+                    
+    function playMove(input,symbol){
+        board[input] = symbol
+    }
 
+    return {board, playMove}
 }
-
-
 
 //GAME logic
 function gameStart(){
@@ -31,16 +35,47 @@ function gameStart(){
         return {name1,name2}
     })();
 
+    const winningPatterns = [
+        [0, 1, 2], // Top row
+        [3, 4, 5], // Middle row
+        [6, 7, 8], // Bottom row
+        [0, 3, 6], // Left column
+        [1, 4, 7], // Middle column
+        [2, 5, 8], // Right column
+        [0, 4, 8], // Top-left to bottom-right diagonal
+        [2, 4, 6]  // Top-right to bottom-left diagonal
+    ];
+
     const player1 = createPlayer(name1,1);
     const player2 = createPlayer(name2,2);
-    const board = createBoard();
+    const {board,playMove} = createBoard();
     db(board);
 
-    board.b5 = player1.symbol;
-    board.b7 = player2.symbol;
-    db(board);
 
-    
+    function checkForWin(symbol){   
+        winningPatterns.some(array=> {
+            if (array.every(value => board[value] === symbol)){
+                if (symbol == player1.symbol){
+                    db(player1.name)
+                    return player1.name
+                }
+                else{
+                    db(player2.name)
+                    return player2.name
+                }
+            }
+            else{
+                return
+            }
+        });}
+
+    playMove(0,player1.symbol);
+    playMove(4,player2.symbol);
+    playMove(1,player1.symbol);
+    playMove(8,player2.symbol);
+    playMove(2,player1.symbol);
+    checkForWin(player1.symbol)
+    console.table(board);
 }
 
 
