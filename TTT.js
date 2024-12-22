30dfa0f31dbb19ddc4e0db6d4663bf9c03aeee6e
Joshua Noyes <Koholos@yahoo.com>
2024-12-22 17:02:01 -0500

    added ability to play turn and check for winner.

--- TTT.js
+++ TTT.js
@@ -24,17 +24,6 @@ function createBoard(){
         board[input] = symbol
     }
 
-    return {board, playMove}
-}
-
-//GAME logic
-function gameStart(){
-    const {name1,name2} = (function(){
-        const name1 = prompt("Player 1's name?").trim();
-        const name2 = prompt("Player 2's name?").trim();
-        return {name1,name2}
-    })();
-
     const winningPatterns = [
         [0, 1, 2], // Top row
         [3, 4, 5], // Middle row
@@ -46,36 +35,69 @@ function gameStart(){
         [2, 4, 6]  // Top-right to bottom-left diagonal
     ];
 
+    function checkForWin(symbol){   
+        return winningPatterns.some(array=> {
+            if (array.every(value => board[value] === symbol)){
+                db("Winner!")
+                return true
+            }
+            return false;
+            });
+    }
+
+
+    return {board,playMove,checkForWin}
+}
+
+//GAME logic
+function gameStart(){
+    const {name1,name2} = (function(){
+        const name1 = prompt("Player 1's name?").trim();
+        const name2 = prompt("Player 2's name?").trim();
+        return {name1,name2}
+    })();
+
+    let turn = 0
+    const startGame = () =>turn;
+
+    function playTurn(event,player){
+        let result = "";
+        turn++;
+        if (turn == 10){
+            result = "Draw!"
+            return result
+        }
+
+        playMove(event,player)
+
+        if (turn >= 5){
+            result = checkForWin(player1.symbol) === true?player1.name + " wins!":"";
+            db(result)
+            return result;
+        }
+        else{
+            return "";
+        }
+    }
+
+
     const player1 = createPlayer(name1,1);
     const player2 = createPlayer(name2,2);
-    const {board,playMove} = createBoard();
-    db(board);
+    const {board,playMove,checkForWin} = createBoard();
+    startGame();
+    db(turn);
 
 
-    function checkForWin(symbol){   
-        winningPatterns.some(array=> {
-            if (array.every(value => board[value] === symbol)){
-                if (symbol == player1.symbol){
-                    db(player1.name)
-                    return player1.name
-                }
-                else{
-                    db(player2.name)
-                    return player2.name
-                }
-            }
-            else{
-                return
-            }
-        });}
-
-    playMove(0,player1.symbol);
-    playMove(4,player2.symbol);
-    playMove(1,player1.symbol);
-    playMove(8,player2.symbol);
-    playMove(2,player1.symbol);
-    checkForWin(player1.symbol)
-    console.table(board);
+    playTurn(0,player1.symbol);
+    db(turn);
+    playTurn(4,player2.symbol);
+    db(turn);
+    playTurn(1,player1.symbol);
+    db(turn);
+    playTurn(8,player2.symbol);
+    db(turn);
+    playTurn(2,player1.symbol);
+    db(turn);
 }
 
 
